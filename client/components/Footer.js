0dd29b9ec70082606940ed065ecd3515cd7876ae
Michael Cuccia <michael.cuccia@us4desm0fg4n0md.home>
2021-06-09 16:51:45 -0400

    created layout, header, and footer

---
+++ client/components/Footer.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Copyright &copy; metuchenUNDERGROUND 2021</p>
+            <p>
+                <Link href='/about'>
+                    About Us
+                </Link>
+            </p>
+        </footer>
+    )
+}
---
+++ client/pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+export default function AboutPage() {
+    return (
+        <Layout
+            title='About metuchenUNDERGROUND'
+        >
+            <h1>About</h1>
+            <p>Find the latest underground events in Metuchen, New Jersey</p>
+            <small>Version 1.0.0</small>
+        </Layout>
+    )
+}
