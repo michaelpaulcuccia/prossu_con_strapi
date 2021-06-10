e955f21679b0e36e9a60d49c2ecbbf6bc392c9bd
Michael Cuccia <michael.cuccia@us4desm0fg4n0md.home>
2021-06-10 09:40:15 -0400

    basic layout

---
+++ client/components/Showcase.js
@@ -0,0 +1,8 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+        </div>
+    )
+}
--- client/pages/about.js
+++ client/pages/about.js
@@ -1,13 +1,16 @@
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout';
+import styles from '@/styles/About.module.css';
 
 export default function AboutPage() {
     return (
         <Layout
             title='About metuchenUNDERGROUND'
         >
-            <h1>About</h1>
-            <p>Find the latest underground events in Metuchen, New Jersey</p>
-            <small>Version 1.0.0</small>
+            <div className={styles.content}>
+                <h1>About</h1>
+                <p>Find the latest underground events in Metuchen, New Jersey</p>
+                <small>Version 1.0.0</small>
+            </div>
         </Layout>
     )
 }
