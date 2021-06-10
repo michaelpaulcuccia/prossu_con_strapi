35aadcd5f02069936fb216c223e0204027874500
Michael Cuccia <michael.cuccia@us4desm0fg4n0md.home>
2021-06-10 14:25:51 -0400

    four events

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,41 @@
 import Layout from '@/components/Layout';
+import {API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
+import Link from 'next/link';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+
   return (
     <Layout>
-      Home
+      <h1>Upcoming</h1>
+      {events.length === 0 && <h3>No Events</h3>}
+      {events.map(evt => (
+        <EventItem 
+        key={evt.id}
+        evt={evt}
+        />
+      ))}
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
+
+}
+
+export async function getStaticProps() {
+
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      //only up to first 3  
+      events:events.slice(0,3)
+    },
+    revalidate: 1
+  }
+
 }
